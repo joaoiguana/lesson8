62ec00089335bd15a9efdc2a4ad3af21e355bd28
Joao Iguana <jiboiadamor@gmail.com>
2024-03-21 09:03:20 +0000

    learned python dictionaries, how to access and manage key-value pairs

---
+++ data_structures.py
@@ -0,0 +1,18 @@
+lort ={"name": "The Fellowship of the Ring","author": "JRR Tolkien", "year": 1954, "genre": "adventure"}
+
+print(lort["author"])
+print("The first book of Lord of the rings is", lort["name"], ", published on", lort["year"])
+
+lort["name"] = "The Two Towers"
+
+print("The second book of Lord of the rings is", lort["name"], ", and it was also published on", lort["year"])
+
+lort["name"] = "Return of the King"
+lort["year"] = 1955
+lort["movie"] = 2003
+
+print("The last book of Lord of the rings is", lort["name"], ", published on", lort["year"], "with a movie was released in", lort["movie"])
+
+del lort["genre"]
+
+print(lort)
